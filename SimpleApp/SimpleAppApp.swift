a38a770f1b0d3d3b7709335e12f2ec2e74f47917
nildavane <nildagregory55@gmail.com>
2023-01-19 21:07:46 -0500

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Nilda Gregory on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
